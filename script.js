c9a8264694a9174c19c15a750631c8f2b123e1ac
Katherine Allen <katherineallen067@gmail.com>
2020-12-04 13:11:48 -0500

    updated to hold answers in ds

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const testOutputs=[];
+
 const calculator = (function(){
     function add(stringNumbers){
       if(stringNumbers === ''){
@@ -5,8 +7,8 @@ const calculator = (function(){
       }
       const delimiter = getDelimiter(stringNumbers)
       const formatValues = formatString(stringNumbers)
-      console.log('the delimiter: ',delimiter)
-      console.log('the formatted values: ',formatValues)
+      // console.log('the delimiter: ',delimiter)
+      // console.log('the formatted values: ',formatValues)
       return calculatedSum(getNumbers(formatValues, delimiter))
     }
       //formatting to digits and char
@@ -14,7 +16,7 @@ const calculator = (function(){
         const reg = /^(\/\/.*\n)/
         const results = reg.exec(items)
           if(results && results.length > 0){
-            console.log('results array :',results)
+            // console.log('results array :',results)
           return items.replace(reg, '')
           }
           return items
@@ -27,15 +29,15 @@ const calculator = (function(){
         while(matches !== null){
             delimiters.push(matches[1]) 
             matches = multipleDelimiterRegexp.exec(items)
-            console.log('return null to break loop: ',matches)
+            // console.log('return null to break loop: ',matches)
           }
           if(delimiters.length > 0){
-            console.log('delimiters: ',delimiters)
+            // console.log('delimiters: ',delimiters)
             return new RegExp('['+delimiters.join('')+']')
           }
           matches = /^\/\/(.*)\n/.exec(items)
           if(matches && matches[1]){
-            console.log('matches without multidelimiters: ', matches)
+            // console.log('matches without multidelimiters: ', matches)
             return matches[1]
           } 
           return /[\n,]/
@@ -66,18 +68,18 @@ const calculator = (function(){
               if (negatives.length > 0){
                 console.error('negatives not allowed')
               }
-              return total;
+              testOutputs.push(total);
+              return total, testOutputs;
         }
       return{add}
 }())
 //examples tests 
-// console.info('test one with empty string: ',calculator.add('')) // 0
-// console.info('test two string with numbers comma separated :', calculator.add('1,2,3')) // 6
-// // console.info('test four ',calculator.add('//;\n1;2;3')) //6
-// console.info('test eight ', calculator.add('//[..][%%]\n1..2%%3')) // 6
-
-// console.info('test three string newline & special characters:',calculator.add('//;\n1;3;4')) // 8
-// console.info('test five ', calculator.add('1001,2')) // 2
-// console.info('test six ', calculator.add('//[**]\n1**2**3')) // 6
-// console.info('test seven ', calculator.add('//[*][%]\n1*2%3')) // 6
-// console.info('test nine ', calculator.add('-1,-3,2')) // 2
+console.info('test one with empty string: ',calculator.add(''), testOutputs) // 0
+console.info('test two string with numbers comma separated :', calculator.add('1,2,3')) // 6
+console.info('test four ',calculator.add('//;\n1;2;3')) //6
+console.info('test eight ', calculator.add('//[..][%%]\n1..2%%3')) // 6
+console.info('test three string newline & special characters:',calculator.add('//;\n1;3;4')) // 8
+console.info('test five ', calculator.add('1001,2')) // 2
+console.info('test six ', calculator.add('//[**]\n1**2**3')) // 6
+console.info('test seven ', calculator.add('//[*][%]\n1*2%3')) // 6
+console.info('test nine ', calculator.add('-1,-3,2')) // 2
